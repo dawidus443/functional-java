cb84381cbf729c21e99b13636d83d932dbbd8e58
Dawid Badora <dawidbadora@gmail.com>
2022-10-09 20:55:45 +0200

    Streams

--- src/main/java/imperative/Main.java
+++ src/main/java/imperative/Main.java
@@ -33,8 +33,8 @@ public class Main {
         }
 
         System.out.println("// Declarative approach");
-        // Declarative approach
 
+        // Declarative approach
 
         Predicate<Person> personPredicate = person -> FEMALE.equals(person.gender);
 
